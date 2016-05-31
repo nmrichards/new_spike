73e1c31d2922355d36c4da8032c7880c5bdd7fd3
chriselevate <coates.c@hotmail.com>
2016-05-31 15:53:03 +0100

    05_controller_expressions

--- controllers.js
+++ controllers.js
@@ -1,3 +1,12 @@
 toDoApp.controller('ToDoController', function() {
-  this.todos = [{task: 'task1', completed: false},{task: 'task2', completed: true}];
+  var self = this;
+
+  self.todos = [{task: 'task1', completed: false}, {task: 'task2', completed: true}];
+
+  self.addToDo = function(newtask) {
+    self.todos.push({task: newtask, completed: false});
+  };
+  self.removeLastTask = function() {
+    self.todos.pop();
+  };
 });
